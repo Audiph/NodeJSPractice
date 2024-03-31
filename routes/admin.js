0f26cadc0c561c38d2256d40ef88090caf764233
audiph <lagarde.marcjoseph@gmail.com>
2024-03-31 11:40:58 +0800

    add controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,10 @@
 const express = require('express');
+const { getAddProduct, postAddProduct } = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    title: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeProduct: true,
-  });
-});
+router.post('/add-product', postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
