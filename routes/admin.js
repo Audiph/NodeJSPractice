1c698ca68d29c24e70cee2b6d590e562623c6f69
audiph <lagarde.marcjoseph@gmail.com>
2024-04-02 02:25:21 +0800

    add product delete

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const {
   getProducts,
   getEditProduct,
   postEditProduct,
+  postDeleteProduct,
 } = require('../controllers/admin');
 
 const router = express.Router();
@@ -18,5 +19,7 @@ router.post('/add-product', postAddProduct);
 router.get('/edit-product/:productId', getEditProduct);
 
 router.post('/edit-product', postEditProduct);
+
+router.post('/delete-product', postDeleteProduct);
 
 module.exports = router;
